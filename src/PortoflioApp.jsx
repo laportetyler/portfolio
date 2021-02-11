53e2f8929a7483c8dc65189319d91ad826dc122f
laportetyler <tylerlaporte@gmail.com>
2021-02-11 17:29:49 -0500

    Adding page loader overlay

--- src/PortoflioApp.jsx
+++ src/PortoflioApp.jsx
@@ -1,6 +1,9 @@
 import React, { useEffect } from 'react'
 import ReactGA from 'react-ga';
 import PortfolioPage from "./pages/PortfolioPage/PortfolioPage";
+import { PuffLoader } from "react-spinners";
+import { Placeholder, Preloader } from "react-preloading-screen";
+import { GrommetTheme } from "./theme/GrommetTheme";
 
 const PortfolioApp = () => {
 
@@ -23,9 +26,19 @@ const PortfolioApp = () => {
     }
 
     const darkThemeEnabled = isDarkThemeEnabled();
+    const backgroundColor = darkThemeEnabled ? GrommetTheme.global.colors.background.dark : GrommetTheme.global.colors.background.light;
+    const standoutColor = darkThemeEnabled ? GrommetTheme.global.colors.standoutColor.dark : GrommetTheme.global.colors.standoutColor.light;
 
     return (
-        <PortfolioPage initialDarkThemeEnabled={darkThemeEnabled} />
+        <React.Fragment>
+            <PortfolioPage initialDarkThemeEnabled={darkThemeEnabled} />
+            <Preloader style={{'background-color': backgroundColor}} >
+                <Placeholder>
+                    <PuffLoader isLoading={true} color={standoutColor} size={100} />
+                </Placeholder>
+            </Preloader>
+        </React.Fragment>
+
     );
 }
 
